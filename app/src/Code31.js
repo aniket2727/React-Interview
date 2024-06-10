6c84e328930b4647b846d5a75c5cb3791b5e331d
aniket2727 <aniketkadam5950@gmail.com>
2024-06-10 19:53:19 +0530

    add chanes

--- app/src/Code31.js
+++ app/src/Code31.js
@@ -1,15 +1,54 @@
+import { useEffect, useState } from "react";
 
+const MainApiCallingCustomHook = ({ url }) => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState('');
+    const [status, setStatus] = useState('not');
 
+    useEffect(() => {
+        const callApiToGetData = async () => {
+            try {
+                const apirawresult = await fetch(url);
+                const result = await apirawresult.json();
+                setData(result);
+            } catch (error) {
+                setError('error');
+                console.error("Error fetching data: ", error);
+            } finally {
+                setStatus('complete');
+            }
+        };
+        callApiToGetData();
+    }, [url]);
 
+    return { data, error, status };
+};
 
+const NewCustomHook = () => {
+    const url = "https://jsonplaceholder.typicode.com/todos/1";
+    const { data, error, status } = MainApiCallingCustomHook({ url });
 
-// this is code written by the aniket kadam
+    if (error === 'error') {
+        return <div><h1>Oops something went wrong</h1></div>;
+    }
 
-const NewCustomHook=()=>{
-    return(
+    if (status !== 'complete') {
+        return <div><h1>Loading.....</h1></div>;
+    }
+
+    return (
         <div>
-          Custom hook code
+            {data && (
+                <div>
+                    {Object.entries(data).map(([key, value]) => (
+                        <div key={key}>
+                            <h1>{value}</h1>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
-    )
-}
-export default NewCustomHook
+    );
+};
+
+export default NewCustomHook;
