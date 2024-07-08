31a638737844052caf94f65aacaf8181a0cb2812
aniket2727 <aniketkadam5950@gmail.com>
2024-07-08 14:13:47 +0530

    add code

---
+++ app/src/Code37.js
@@ -0,0 +1,17 @@
+
+
+
+// this code is written by the aniket kadam
+
+
+const Updatedata=()=>{
+    
+    return(
+        <div>
+           <h1>Update Data</h1>
+        </div>
+    )
+};
+
+
+export default Updatedata;
